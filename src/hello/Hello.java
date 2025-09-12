0c44f3405eee33bc950d99186ee6db1d3db2cf66
Varshitha Korepu <varshithakorepu1105@gmail.com>
2025-09-12 13:47:26 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,5 +3,8 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI!");
     System.out.println("DevOps Class");
+    int a = 5;
+    System.out.println(a);
+    System.out.println("Checking Webhook");
   }
 }
